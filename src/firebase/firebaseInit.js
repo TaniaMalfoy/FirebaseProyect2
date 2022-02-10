57fb58489a6cfe6ae51ddb14b02b95b09d69e5de
TaniaMalfoy <39595868+TaniaMalfoy@users.noreply.github.com>
2022-02-10 01:01:29 +0000

    Feb 9th
    Feb 9th

--- src/firebase/firebaseInit.js
+++ src/firebase/firebaseInit.js
@@ -1,9 +1,11 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { FieldValue, getFirestore } from 'firebase/firestore'
+import { getStorage } from "firebase/storage";
+
 
 const firebaseConfig = {
-//firebase Proyect configuration
+  // Config Proyect
   };
 
   // Initialize Firebase / Auth / Firestore
@@ -11,10 +13,14 @@ const firebaseApp = initializeApp(firebaseConfig);
 const auth = getAuth();
 const db = getFirestore();
 
+// Get a reference to the storage service, which is used to create references in your storage bucket
+const storage = getStorage(firebaseApp);
+
+
 //https://firebase.google.com/docs/firestore/manage-data/add-data#server_timestamp
 //const timestamp = FieldValue.serverTimestamp();
 
 // Colletion reference
 //const colRef = collection(db,'Post');
 
-export  { auth, db, firebaseApp }
+export { auth, db, firebaseApp, storage }
